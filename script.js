0f6f0a22b44bb72f9697c2655e04551ac541ebf5
Adina CHirila <47609475+Galssy@users.noreply.github.com>
2021-02-13 15:09:05 +0200

    added slider and input validation functionality

--- script.js
+++ script.js
@@ -32,9 +32,9 @@ const updateSubscribeMessage = (message) => {
 
 const validateEmail = (e) => {
 	e.preventDefault();
-	const inputValue = subscribeInput.value;
+	const validationPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-	if (inputValue && inputValue.includes("@")) {
+	if (validationPattern.test(subscribeInput.value)) {
 		updateSubscribeMessage("Successfully subscribed to newsletter");
 		subscribeInput.value = "";
 		subscribeInput.classList.remove("subscribe-error-border");
@@ -43,6 +43,41 @@ const validateEmail = (e) => {
 		subscribeInput.classList.add("subscribe-error-border");
 	}
 };
+
+new Glider(document.querySelector(".glider"), {
+	// mobile
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	scrollLock: true,
+	draggable: true,
+	dots: "#resp-dots",
+
+	responsive: [
+		{
+			// screens greater than >= 700px
+			breakpoint: 700,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				draggable: true,
+				itemWidth: 150,
+				duration: 0.25,
+			},
+		},
+
+		{
+			// screens greater than >= 1000px
+			breakpoint: 1000,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				draggable: true,
+				itemWidth: 150,
+				duration: 0.25,
+			},
+		},
+	],
+});
 
 burgerBtn.addEventListener("click", showBurgerMenu);
 backdrop.addEventListener("click", closeNav);
